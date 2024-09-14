8529ecdb1e2e2a2cd34fff5ef7b1888e6d12ee02
Sivateja26 <gaddesivateja@gmail.com>
2024-09-14 15:22:52 +0530

    Chatbot prgms

---
+++ reverse.js
@@ -0,0 +1,11 @@
+function reverseEachWord(str) {
+    return str.split(' ')
+              .map(word => word.split('').reverse().join(''))
+              .join(' ');
+}
+
+// Example usage:
+let input = prompt("Enter a string:");
+let output = reverseEachWord(input);
+console.log("Input:", input);
+console.log("Output:", output);
